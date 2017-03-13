bf74e478737c27b7b26107d3ce56882fcd3b86b1
pyotr <pyotr@DESKTOP-P1TTQLF>
2017-03-13 21:06:15 +0200

    .

--- rhombus/src/rhombus/AplusB.java
+++ rhombus/src/rhombus/AplusB.java
@@ -1,13 +1,10 @@
 package rhombus;
 
 public class AplusB implements InterfaceA, InterfaceB {
-    /*public void sayWord(String word) {
-    	InterfaceB.sayWord(word);
-	}*/
-
-    /*@Override
     public void sayWord(String word) {
-        System.out.println("doSomething реализация реального класса ");
-    }*/
+    	InterfaceB.sayWord(word);
+	}
+
+
 
 }
--- rhombus/src/rhombus/Runner.java
+++ rhombus/src/rhombus/Runner.java
@@ -4,10 +4,8 @@ public class Runner {
 
 	public static void main(String[] args) {
 		AplusB  rhomb  = new AplusB();
-		//InterfaceB aaa = new InterfaceB();
 		
-		//((InterfaceB)rhomb).sayWord("aaa");
-        rhomb(InterfaceB).sayWord("hi");
+        rhomb.sayWord("hi");
 	}
 
 }
